e2ac585da52d5376e6f140f44c5f5768a8ea63bb
iyk-azorji <iykazorji@gmail.com>
2018-05-01 12:18:49 +0100

    Configured Google auth

---
+++ backend/src/authentication.js
@@ -0,0 +1,41 @@
+const authentication = require('@feathersjs/authentication');
+const jwt = require('@feathersjs/authentication-jwt');
+const local = require('@feathersjs/authentication-local');
+const oauth2 = require('@feathersjs/authentication-oauth2');
+const GoogleStrategy = require('passport-google-oauth20');
+
+module.exports = function (app) {
+  const config = app.get('authentication');
+
+  // Set up authentication with the secret
+  app.configure(authentication(config));
+  app.configure(jwt());
+  app.configure(local());
+
+  app.configure(oauth2(Object.assign({
+    name: 'google',
+    Strategy: GoogleStrategy
+  }, config.google)));
+
+  // The `authentication` service is used to create a JWT.
+  // The before `create` hook registers strategies that can be used
+  // to create a new valid JWT (e.g. local or oauth2)
+  app.service('login').hooks({
+    before: {
+      create: [
+        authentication.hooks.authenticate(config.strategies)
+      ],
+      remove: [
+        authentication.hooks.authenticate('jwt')
+      ]
+    },
+    after: {
+      create: [
+        context =>{
+          context.result.user = context.params.user;
+          delete context.result.user.password  
+        }
+      ]
+    }
+  });
+};
